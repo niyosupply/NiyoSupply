01473372cf917cd8394f8a2ab3b6c4df832613c9
niyosupply <niyosupply04@gmail.com>
2026-03-21 10:12:55 +0800

    footer

---
+++ niyosupply/public/components/footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <p><strong>NiyoSupply</strong></p>
+      <p>For educational purposes only, and no copyright infringement is intended.</p>
+    </footer>
+  );
+}
+
+export default Footer;
